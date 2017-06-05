1878f770f70329cadf0ebed822b7ef5168d6f3a7
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-05 12:35:26 +0300

    Created children component in router /greet

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,20 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () =>{
+    return <div>Hey there!</div>;
+};
+
 export default(
-    <Route path="/" component={App} />
+    <Route path="/" component={App} >
+        <Route path="greet" component={Greeting} />
+        <Route path="greet2" component={Greeting} />
+        <Route path="greet3" component={Greeting} />
+    </Route>
 );
 //google.com/ ==> render App
+
+//      /       App
+//      /greet  App,Greeting
+//      /greet2 App,Greeting
+//      /greet3 App,Greeting
