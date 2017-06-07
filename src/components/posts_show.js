b15081bd676986272188b51cb7f072f755a04d1d
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-08 00:55:14 +0300

    Create axios on one post and made true data request single post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component{
+    componentWillMount(){
+        this.props.fetchPost(this.props.params.post_id);
+    }
+    
     render(){
         return (
         <div>
@@ -10,4 +16,4 @@ class PostsShow extends Component{
     }
 }
 
-export default PostsShow;
+export default connect(null,{fetchPost})(PostsShow);
