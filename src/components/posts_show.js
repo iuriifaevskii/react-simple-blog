078366531dab5ce6b021e4c7cd95b9baac711803
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-08 01:21:16 +0300

    Showed single post data on screen and solved the problem with Null

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,12 +8,27 @@ class PostsShow extends Component{
     }
     
     render(){
+        const {post} = this.props;
+        //перевірка треба бо загружається null на екран
+        //і не встигає загрузитись контент, тому поки null виводити треба лоадер
+        if(!this.props.post){
+            return <div>Loading...</div>
+        }
         return (
         <div>
-            Show Post {this.props.params.post_id}
+            <h3>{post.title}</h3>
+            <h6>Categories: {post.categories}</h6>
+            <p>{post.content}</p>
         </div>
         );
     }
 }
 
-export default connect(null,{fetchPost})(PostsShow);
+function mapStateToProps(state){
+    console.log(state.posts.post);
+    return {
+        post: state.posts.post
+    };
+}
+
+export default connect(mapStateToProps,{fetchPost})(PostsShow);
