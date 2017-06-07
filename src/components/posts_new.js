b9cef9583b6b2dedf117b86a235c3596893ebfcf
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-07 20:47:54 +0300

    Write all props (title,categories,content)  in component, Add onSubmit in form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,31 +3,44 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render(){
+  //    //const { fields:{title,categories,content}, handleSubmit } = this.props;
+        //те ж саме: 
+        const handleSubmit = this.props.handleSubnit;
+        const title = this.props.fields.title;
+        const categories = this.props.fields.categories;
+        const content = this.props.fields.content;
+        
+        console.log(title);
+        //{...title}
+        //formProps = {title} // => ...title
+        //onChange = {title.onChange} 
+
         return(
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" {...title} />
                 </div>
                 <div className="form-group">
                     <label>Categories</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" {...categories} />
                 </div>
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea className="form-control" />
+                    <textarea className="form-control" {...content}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
-
+//пишемо конфігурації до нашої редакс форми
 export default reduxForm({
-    form: 'PostsNewForm',
+    form: 'PostsNewForm', // ім*я форми це унікальний токен
     fields: ['title','categories','content']
 })(PostsNew);
+
 //user types smth in.. record it on application state
 //три рядки вище приблизно те ж що і:
 // state === {
