cef60758e1904dda0d86117e58c09673d7724758
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-07 11:35:42 +0300

    Install axios and promise. Created axios request all post and transferred it in reducer_posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => {
-    return <div>List of blog posts</div>
+class PostsIndex extends Component{
+    componentWillMount(){
+        console.log('this would be a good time to call an action creater fetch posts');
+    }
+    //
+    //componentWillMount - компонент будет примонтирован. 
+    //В данный момент у нас нет возможности посмотреть DOM элементы
+    //
+    
+    render(){
+        return(
+            <div>List of blogs</div>
+        );
+    }
 }
+
+export default PostsIndex;
