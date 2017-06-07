4ef274422cbc2c86882cfb07d0bf3795b22101a1
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-08 01:46:52 +0300

    Create delete request and button

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component{
     componentWillMount(){
         this.props.fetchPost(this.props.params.post_id);
     }
     
+    onDeleteClick(){
+        this.props.deletePost(this.props.params.post_id);
+    }
+    
     render(){
         const {post} = this.props;
-        //перевірка треба бо загружається null на екран
+        //перевірка треба, бо загружається null на екран
         //і не встигає загрузитись контент, тому поки null виводити треба лоадер
         if(!this.props.post){
             return <div>Loading...</div>
         }
         return (
         <div>
+            <Link to="/">Back To Index</Link>
+            <button 
+                className="btn btn-danger pull-xs-right"
+                onClick={this.onDeleteClick.bind(this)}
+            >
+                Delete Post
+            </button>
             <h3>{post.title}</h3>
             <h6>Categories: {post.categories}</h6>
             <p>{post.content}</p>
@@ -31,4 +43,4 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps,{fetchPost})(PostsShow);
+export default connect(mapStateToProps,{fetchPost, deletePost})(PostsShow);
